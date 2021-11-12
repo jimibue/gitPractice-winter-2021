4641e5d9b3a1e6b1496863fc1b5cd965de42b858
James Yeates <jimibue@gmail.com>
2021-11-12 09:50:37 -0800

    intial commit

--- demo.rb
+++ demo.rb
@@ -1,4 +1,14 @@
 puts "hello world"
-
 puts "1+1 = 2"
-puts ""
+puts "Hello from vim"
+
+# setup do this for each project
+# git init to create a repo
+# create a repo on github
+# add the remote link
+
+# basic flow
+# make change in your code
+# add(stage) those changes git add .   or git add filename
+# commit those changes git commit -m'description of what you did'
+# push up to github
